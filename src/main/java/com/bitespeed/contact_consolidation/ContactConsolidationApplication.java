3e7c2c08b1533b1fcfd669fd35c796c3f9ccffc6
sulthanasyed <syednageenasulthana@gmail.com>
2025-05-17 11:36:51 +0530

    Initial commit

---
+++ src/main/java/com/bitespeed/contact_consolidation/ContactConsolidationApplication.java
@@ -0,0 +1,13 @@
+package com.bitespeed.contact_consolidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactConsolidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactConsolidationApplication.class, args);
+	}
+
+}
